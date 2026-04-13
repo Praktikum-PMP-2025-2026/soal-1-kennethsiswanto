32e86faa086a66f5134d7e8fe7ddc8bedd70b083
kennethsiswanto <kennethsiswanto@gmail.com>
2026-04-13 08:44:37 +0700

    Update soal1.c

--- soal1.c
+++ soal1.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
 
-int main(void) {
-    return 0;
+int main(){
+    int izin, radiasi, jam;
+    float suhu;
+    scanf("%d %f %d %d", &izin, &suhu, &radiasi, &jam);
+    if(radiasi >= 6){
+        printf("TOLAK");
+        return 0;
+    }
+    if(suhu >= 390){
+        printf("KARANTINA");
+        return 0;
+    }
+    if(izin == 1 && jam < 6 && jam > 20){
+        printf("TOLAK");
+        return 0;
+    }
+    if(izin == 1){
+        printf("MASUK");
+        return 0;
+    }
+    if(izin == 2 && radiasi <= 2 && jam>8 && jam<18){
+        printf("MASUK");
+        return 0;
+    }
+    if(izin == 2){
+        printf("PEMERIKSAAN");
+        return 0;
+    }
+    if(izin == 3 && radiasi == 0 && suhu<380){
+        printf("MASUK");
+        return 0;
+    }
+    else{
+        printf("TOLAK");
+        return 0;
+    }
+
+    // tes input per variabel
+    // printf("izin = %d\n", izin);
+    // printf("suhu = %f\n", suhu);
+    // printf("radiasi = %d\n", radiasi);
+    // printf("jam = %d\n", jam);
 }
